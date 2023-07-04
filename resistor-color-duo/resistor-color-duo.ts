1c20d8d304fd19762ca92c8646cd868b30c97f9e
Igor Radovanov <igor@radovanovigor.com>
2023-07-04 17:14:18 +0200

    Color resistor controller

---
+++ resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,30 @@
+export function decodedValue([firstBand, secondBand, thirdBand]: [string, string, string?]): number {
+
+
+  const resistanceValues = new Map<string, number>([
+    ["black", 0],
+    ["brown", 1],
+    ["red", 2],
+    ["orange", 3],
+    ["yellow", 4],
+    ["green", 5],
+    ["blue", 6],
+    ["violet", 7],
+    ["grey", 8],
+    ["white", 9]
+  ]);
+
+
+  if (!resistanceValues.has(firstBand) || !resistanceValues.has(secondBand)) {
+    throw new Error("Invalid color bands provided");
+  }
+
+  const value1 = resistanceValues.get(firstBand)!;
+  const value2 = resistanceValues.get(secondBand)!;
+
+  if (firstBand === "black") {
+    return value2;
+  } else {
+    return value1 * 10 + value2;
+  }
+}
